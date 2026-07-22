8bc74a8f04cb45e2d354b9ca84ea686948959afb
ThePlatinumDragon999 <flailsnail999@gmail.com>
2026-07-22 15:37:15 -0500

    Added documentation for the Zagier pair visualization code

--- Windmill/windmill.py
+++ Windmill/windmill.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 # prime of form 4k+1
-p = 37
+p = 41
 
-# Generate all natural solutions (x, y, z)
+# Generate all natural solutions (x, y, z) such that x^2 + 4yz = p
 S = []
 for y in range(1, p):  # positive integers
     for x in range(1, p):
@@ -23,33 +23,36 @@ def zagier(x, y, z):
         return (x - 2*y, z + x - y, y)  # left region
     elif (y > x + z):
         return (x + 2*z, z, y - x - z)  # right region
-    else:
-        return None
 
 # Plot
 fig = plt.figure(figsize=(10,8))
 ax = fig.add_subplot(111, projection='3d')
 
-colors = {'central':'skyblue', 'left':'lightgreen', 'right':'orange'}
+colors = {'central':'#9ab5ff', 'left':'#87ff78', 'right':'#b00b69'}
 
+# Iterate over each point in the list of psuedo-Pythagorean triples
+# For each point, calculate its Zagier involution image
+# It then determines which case the point fell into.
+# It then plots the point, then draws an arrow to its image (as long as not fixed point)
 for point in S:
     x, y, z = point
-    target = zagier(x, y, z)
-    if target:
-        x_new, y_new, z_new = target
-        # Determine region
-        if (2*y > x) and (z + x > y):
-            region = 'central'
-        elif (x > 2*y) and (z + x > y):
-            region = 'left'
-        else:
-            region = 'right'
-        # Plot point
-        ax.scatter(x, y, z, color=colors[region], s=50)
-        # Draw arrow to its image
-        if target and (x_new, y_new, z_new) != (x, y, z):
-            ax.quiver(x, y, z, x_new-x, y_new-y, z_new-z,
-                      color=colors[region], length=1.0, normalize=True, alpha=0.5)
+    x_new, y_new, z_new = zagier(x, y, z)
+    
+    # Determine region
+    if (2*y > x) and (z + x > y):
+        region = 'central'
+    elif (x > 2*y) and (z + x > y):
+        region = 'left'
+    else:
+        region = 'right'
+    
+    # Plot point
+    ax.scatter(x, y, z, color=colors[region], s=50)
+    
+    # Draw arrow to its image
+    if (x_new, y_new, z_new) != (x, y, z):
+        ax.quiver(x, y, z, x_new-x, y_new-y, z_new-z,
+              color=colors[region], length=1.0, normalize=True, alpha=0.5)
 
 # Plot fixed point
 fixed_point = (1, 1, (p - 1)//4)
