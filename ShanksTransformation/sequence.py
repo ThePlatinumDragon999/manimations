040bae0e3d329c0ad583cb291bcf67665b1ab291
ThePlatinumDragon999 <flailsnail999@gmail.com>
2026-08-11 12:59:06 -0600

    Refactored Leibniz partial sum method to avoid redudant calculations

--- ShanksTransformation/sequence.py
+++ ShanksTransformation/sequence.py
@@ -1,7 +1,7 @@
-from math import *
+from math import pi
 from manim import *
 
-class Test(Scene):
+class Shanks(Scene):
     def construct(self):
 
         # Set up the plane
@@ -10,23 +10,34 @@ class Test(Scene):
             ).add_coordinates()
         )
 
-        # Compute partial sums of the Leibniz series
-        def leibniz_partial_sum(n):
-            return 4 * sum(((-1) ** k) / (2 * k + 1) for k in range(n + 1))
+        # Compute all Leibniz partial sums incrementally
+        partial_sums = []
+        current_sum = 0
+
+        for n in range(60):
+            current_sum += ((-1) ** n) / (2 * n + 1)
+            partial_sums.append(4 * current_sum)
 
         # Create dots for each partial sum
         dots = VGroup(
-            *[Dot(plane.c2p(n, leibniz_partial_sum(n)), color=RED, radius=.05) for n in range(1, 60)]
+            *[
+                Dot(
+                    plane.c2p(n, partial_sums[n]),
+                    color=RED,
+                    radius=.05
+                ) 
+                for n in range(1, 60)
+            ]
         )
         
-        # Add a line at y = pi/4 for reference
+        # Add a line at y = pi for reference
         pi_line = DashedLine(
             start=plane.c2p(0, pi),
             end=plane.c2p(60, pi),
             color=YELLOW
         )
         
-        pi_label = MathTex("\pi").next_to(pi_line, RIGHT)
+        pi_label = MathTex(r"\pi").next_to(pi_line, RIGHT)
 
         # Add the plane and animate everything
         self.play(Create(plane))
