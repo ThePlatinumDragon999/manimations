4cc25feaec94e736d06de63b25108b2fa08e5398
ThePlatinumDragon999 <flailsnail999@gmail.com>
2026-08-22 15:20:19 -0600

    Added triangle centers

--- NapoleonsTheorem/triangle_centers.py
+++ NapoleonsTheorem/triangle_centers.py
@@ -30,14 +30,67 @@ class TriangleCenters(Scene):
 
             triangles.append(triangle)
 
-        # TODO: centers
+        # Calculate centers
+        for position in centers:
+            a = A + position
+            b = B + position
+            c = C + position
+
+            # Centroid
+            centroid = (a + b + c) / 3
+
+            # Circumcenter
+            circumcenter = self.get_circumcenter(a, b, c)
+
+            # Incenter
+            side_a = np.linalg.norm(b - c)
+            side_b = np.linalg.norm(a - c)
+            side_c = np.linalg.norm(a - b)
+
+            incenter = (
+                side_a * a + side_b * b + side_c * c
+            ) / (side_a + side_b + side_c)
+
+            # Orthocenter
+            orthocenter = a + b + c - 2 * circumcenter
+
+            center_dots.append([
+                Dot(centroid),
+                Dot(circumcenter),
+                Dot(incenter),
+                Dot(orthocenter)
+            ])
+
+        # Pick a center for each triangle
+        dots = [
+            center_dots[0][0], # Centroid
+            center_dots[1][1], # Circumcenter
+            center_dots[2][2], # Incenter
+            center_dots[3][3] # Orthocenter
+        ]
 
         # Animation
         self.play(
-            LaggedStart(
-                *[Create(triangle) for triangle in triangles],
-                lag_ratio = 0.5
-            )
+            *[FadeIn(triangle) for triangle in triangles]
+        )
+
+        self.play(
+            *[FadeIn(dot) for dot in dots]
         )
 
         self.wait()
+
+    def get_circumcenter(self, A, B, C):
+        M = np.array([
+            2 * (B - A)[:2],
+            2 * (C - A)[:2]
+        ])
+
+        rhs = np.array([
+            np.dot(B, B) - np.dot(A, A),
+            np.dot(C, C) - np.dot(A, A)
+        ])
+
+        center = np.linalg.solve(M, rhs)
+
+        return np.array([center[0], center[1], 0])
